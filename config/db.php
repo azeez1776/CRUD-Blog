fa918a0fbc458d92f5b1b401a1c176050544e0c0
Samatar Abdi <39125180+azeez1776@users.noreply.github.com>
2022-01-05 11:03:59 +0300

    Update db.php
    removed password

--- config/db.php
+++ config/db.php
@@ -1,6 +1,6 @@
 <?php
 # making connection with the database
-$conn = mysqli_connect('localhost', 'root', 'samatar@1776', 'Library');
+$conn = mysqli_connect('localhost', 'root', <password>, 'Library');
 
 if (mysqli_connect_errno()) {
     echo 'Something is wrong, check this message ' . mysqli_connect_errno();
